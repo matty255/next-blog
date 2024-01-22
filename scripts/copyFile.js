e17d328ff8ab7fa449c74e9962ec4217fc644610
HR. Lee <matt14507@gmail.com>
2024-01-22 16:23:36 +0900

    fix: copy file script

---
+++ scripts/copyFile.js
@@ -0,0 +1,18 @@
+// scripts/copyFile.js
+
+const fs = require('fs');
+const path = require('path');
+
+const copyFile = () => {
+  const sourcePath = path.join(__dirname, '../local/path/to/song.mp3');
+  const destPath = path.join(__dirname, '../public/song.mp3');
+
+  try {
+    fs.copyFileSync(sourcePath, destPath);
+    console.log('파일이 성공적으로 복사되었습니다.');
+  } catch (error) {
+    console.error('파일 복사 중 오류 발생:', error);
+  }
+};
+
+copyFile();
