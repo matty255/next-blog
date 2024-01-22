985a9fec938cc9d304b2b5a5684c83cc5ed31bfa
HR. Lee <matt14507@gmail.com>
2024-01-22 16:42:31 +0900

    fix: test commit.

--- scripts/copyFile.js
+++ scripts/copyFile.js
@@ -7,6 +7,9 @@ const copyFile = () => {
   const sourcePath = path.join(__dirname, '../posts/radio/I_Want_To_Live.mp3');
   const destPath = path.join(__dirname, '../public/I_Want_To_Live.mp3');
 
+  console.log(`Source Path: ${sourcePath}`);
+  console.log(`Destination Path: ${destPath}`);
+
   try {
     fs.copyFileSync(sourcePath, destPath);
     console.log('파일이 성공적으로 복사되었습니다.');
